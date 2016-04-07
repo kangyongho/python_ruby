d40cc873f75fcf77ec49a4bfc2f0e64b1b932d65
daniel <daniel@danielui-MacBook-Pro.local>
2016-04-07 19:03:44 +0900

    no message

---
+++ Conditional_Statements/5.py
@@ -0,0 +1,10 @@
+input = "ab"
+real_egoing = 11
+real_k8805 = "ab"
+
+if real_egoing == input:
+    print("Hello!, egoing")
+elif real_k8805 == input: #else if = elif
+    print("Hello!, k8805")
+else:
+    print("Who are you")
