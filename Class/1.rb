18017f5311a6c73ac7779c03038cdb76c2f39947
kangyongho <postmirlang2@gmail.com>
2016-04-17 22:10:58 +0900

    생활코딩

---
+++ Class/1.rb
@@ -0,0 +1,24 @@
+class Cal
+# 생성자, constructor
+  def initialize(v1, v2)
+    p v1, v2
+    @v1 = v1 # 인스턴스변수 : 전역변수가 된다. @를 붙이면
+    @v2 = v2
+  end
+
+  def add()
+    return @v1 + @v2
+  end
+
+  def subtract()
+    return @v1 - @v2
+  end
+end
+
+c1 = Cal.new(10, 10)
+p c1.add()
+p c1.subtract()
+
+c2 = Cal.new(30, 20)
+p c2.add()
+p c2.subtract()
---
+++ Class/2.rb
@@ -0,0 +1,12 @@
+def add(v1, v2)
+  return v1 + v2
+end
+
+def substract(v1, v2)
+  return v1 - v2
+end
+
+p add(10, 10)
+p substract(10, 10)
+p add(30, 20)
+p substract(30, 20)
